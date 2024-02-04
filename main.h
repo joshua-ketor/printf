7ab77b648147086edc59085f876a9f5fba0b61c2
Joshua Ketor <joshuaketor@proton.me>
2024-02-04 00:37:37 +0000

    Update: main.h

--- main.h
+++ main.h
@@ -1,6 +1,13 @@
 #ifndef MAIN_H
 #define MAIN_H
 
+#include <unistd.h>
+#include <stdlib.h>
+#include <stdarg.h>
+
 int _putchar(char c);
+void _write_string(char *str);
+void _write_int(int num);
+void _printf(const char *format, ...);
 
 #endif /* MAIN_H */
