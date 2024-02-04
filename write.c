1c8628fd3999fe746b131ef8cd93dddf02858095
Joshua Ketor <joshuaketor@proton.me>
2024-02-04 00:37:58 +0000

    Add: write.c

---
+++ write.c
@@ -0,0 +1,51 @@
+#include "main.h"
+
+/**
+ * _write_string - write string to stdout
+ * @str: pointer to string
+ *
+ * Return: Nothing
+ */
+void _write_string(char *str)
+{
+	while (*str)
+	{
+		_putchar(*str);
+		str++;
+	}
+}
+
+/**
+ * _write_int - write int to stdou
+ * @num: num to write
+ *
+ * Return: Nothing
+ */
+void _write_int(int num)
+{
+	if (num == 0)
+	{
+		_putchar('0');
+		return;
+	}
+
+	if (num < 0)
+	{
+		_putchar('-');
+		num = -num;
+	}
+
+	char *buffer = malloc(sizeof(int) * 3);
+	char *p = buffer + 11;
+	*p = '\0';
+
+	while (num > 0)
+	{
+		p--;
+		*p = '0' + num % 10;
+		num /= 10;
+	}
+
+	_write_string(p);
+	free(buffer);
+}
